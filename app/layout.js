bb7ecb87ab4703b6d56a6ceb89af76d28e495096
arsahak <arsahak99@gmail.com>
2024-10-06 17:36:43 +0600

    update og image

--- app/layout.js
+++ app/layout.js
@@ -1,40 +1,40 @@
-import { Roboto } from 'next/font/google';
-import './globals.css';
-import { Providers } from './providers';
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import MainNavbar from '@/components/MainNavbar';
-import Footer from '@/components/Footer';
+import { Roboto } from "next/font/google";
+import "./globals.css";
+import { Providers } from "./providers";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import MainNavbar from "@/components/MainNavbar";
+import Footer from "@/components/Footer";
 
 const roboto = Roboto({
-  subsets: ['latin'],
-  variable: '--font-roboto',
-  weight: ['100', '300', '400', '700', '900'],
+  subsets: ["latin"],
+  variable: "--font-roboto",
+  weight: ["100", "300", "400", "700", "900"],
 });
 
 export const metadata = {
   title:
-    'Florida Immigration Lawyer Firm || USA Immigration Attorney Hardam Tripathi, Esq.',
+    "Florida Immigration Lawyer Firm || USA Immigration Attorney Hardam Tripathi, Esq.",
   description:
-    'Florida Immigration Law Services Call our local Immigration office for your Green Card, Naturalization or Investor Visa.',
-  metadataBase: new URL('https://trip-law-new-website.vercel.app'),
+    "Florida Immigration Law Services Call our local Immigration office for your Green Card, Naturalization or Investor Visa.",
+  metadataBase: new URL("https://triplaw-milwaukee-legal-website.vercel.app/"),
   alternates: {
-    canonical: '/',
+    canonical: "/",
     languages: {
-      'en-US': '/en-USA',
+      "en-US": "/en-USA",
     },
   },
   openGraph: {
-    images: '/opengraph-image.jpg',
+    images: "/opengraph-image.jpg",
   },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={roboto.className}>
         <Providers>
-          <div className='overflow-x-hidden text-black bg-white'>
+          <div className="overflow-x-hidden text-black bg-white">
             <MainNavbar />
             {children}
             <Analytics />
