76651e2cd943b179889b19dda36af3643da3a850
arsahak <arsahak99@gmail.com>
2025-10-29 16:35:54 +0600

    update blog page

--- components/services/HeroSection.jsx
+++ components/services/HeroSection.jsx
@@ -1,16 +1,10 @@
-import React from 'react';
-import SectionLayout from '../shared/SectionLayout';
-import Image from 'next/image';
-import { Mulish, Bitter } from 'next/font/google';
-import MotionEffect from '../motion/MotionEffect';
-import { Breadcrumbs, BreadcrumbItem } from '@nextui-org/react';
-import PageHeroSection from '../shared/PageHeroSection';
+import PageHeroSection from "../shared/PageHeroSection";
 
 const HeroSection = () => {
   return (
     <PageHeroSection
-      image={'/assets/hero-img/services.jpg'}
-      title={'Services'}
+      image={"/assets/hero-img/services.jpg"}
+      title={"Services"}
     />
   );
 };
